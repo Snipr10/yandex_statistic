96468eb90ddb6e7d3fbca92656b94980da3711c5
oleg <oleg.gusev@mati.io>
2021-11-07 17:57:10 +0300

    new key

--- core/utils.py
+++ core/utils.py
@@ -10,7 +10,7 @@ from core.models import YandexStatistic, YandexStatistic0
 
 DATA_URL = "https://yandex.ru/news/top/region/Saint_Petersburg"
 DATA_TEXT = "window.Ya=window.Ya||{};window.Ya.Neo=window.Ya.Neo||{};window.Ya.Neo.dataSource="
-KEY = "ca93f5d4f09629870d64a2242ec9b61c"
+KEY = "119c6b62e9d9e6bb87ef52bf4e9a6078"
 PROXIES = []
 
 
