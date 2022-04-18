70c9b14c47f502639b1b75d2c004b04e74ece7fe
oleg <gusevoleg@gmail.com>
2022-04-18 19:29:59 +0300

    fix login

--- yandex_statistic/tasks.py
+++ yandex_statistic/tasks.py
@@ -11,13 +11,14 @@ def start_task_new_update():
     print("start")
     get_yandex_data()
 
+
 @app.task
 def start_task():
     import requests
     try:
         print(requests.post("http://127.0.0.1:6000/api/text", data=json.dumps({
-  "urls": "https://spb.dixinews.ru/news/transport/v-peterburge-startuet-rechnaya-navigatsiya/"
-})))
+            "urls": "https://spb.dixinews.ru/news/transport/v-peterburge-startuet-rechnaya-navigatsiya/"
+        })))
         print(requests.post("http://127.0.0.1:6000/api/text", data=json.dumps({
             "urls": "https://spb.dixinews.ru/news/transport/v-peterburge-startuet-rechnaya-navigatsiya/"
         })).text)
