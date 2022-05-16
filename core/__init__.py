768d02501bd42d9bcd9992b4e433db32465686fd
oleg <gusevoleg@gmail.com>
2022-05-16 22:28:56 +0300

    fix

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
+from __future__ import absolute_import
+
 try:
-    from __future__ import absolute_import
     from .celery import app as celery_app
     __all__ = ('celery_app',)
 except Exception as e:
