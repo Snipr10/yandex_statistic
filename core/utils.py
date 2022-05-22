6b02b49cdab2c84d7dcc89fb563dcd278b27a932
oleg.gusev <gusevoleg@gmail.com>
2022-05-22 14:58:56 +0300

    fix

--- core/utils.py
+++ core/utils.py
@@ -89,29 +89,34 @@ def get_response(new_session):
 
 
 def get_response_news(new_session, url):
-    headers = {
-        'authority': 'yandex.ru',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'ru-RU,ru;q=0.9',
-        'dnt': '1',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'
-    }
+    new_response = None
+    while True:
+        headers = {
+            'authority': 'yandex.ru',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'ru-RU,ru;q=0.9',
+            'dnt': '1',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'
+        }
 
-    try:
-        new_response = new_session.get(url, headers=headers).text
-    except Exception as e:
-        return get_response_news(get_proxy(), url)
-    if "captcha" in new_response:
-        new_session = get_proxy()
-        return get_response_news(new_session, url)
-    return new_response, new_session
+        try:
+            new_response = new_session.get(url, headers=headers).text
+        except Exception as e:
+            print(f"get_response_news {e}")
+            # return get_response_news(get_proxy(), url)
+        if "captcha" in new_response:
+            new_session = get_proxy()
+            # return get_response_news(new_session, url)
+        if new_response:
+            break
+        return new_response, new_session
 
 
 def get_yandex_data(session=None):
